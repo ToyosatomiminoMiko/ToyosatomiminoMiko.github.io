cd4ff5679bbe7bc3669809d5fc17879dac4b4ed3
haojiren galactic <603600299@qq.com>
2026-08-15 06:40:16 +0800

    清理旧方法

--- math-lab/src/ui/detail/utils.ts
+++
@@ -1,6 +0,0 @@
-/** HTML转义字符串:Anti-XSS */
-export function escapeHtml(str: string): string {
-    const div = document.createElement('div');
-    div.textContent = str;
-    return div.innerHTML;
-}
