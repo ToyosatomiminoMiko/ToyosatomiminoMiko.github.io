d149d30e182e856b09e993864779b2167d39b10c
haojiren galactic <603600299@qq.com>
2026-09-13 20:35:59 +0800

    项目分离

--- graphcalc/src/compiler/compiler_rs/src/lib.rs
+++
@@ -1,8 +0,0 @@
-pub mod parser_wasm;
-
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn parse_miko(source: &str) -> Result<String, JsValue> {
-    parser_wasm::parse_to_json(source).map_err(|e| JsValue::from_str(&e))
-}
